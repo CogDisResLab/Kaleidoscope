8811f65ede1d41742403bf9d7ec44dec28b7397b
rela <rela.mngrbot@gmail.com>
2024-05-16 13:26:08 -0400

    changed main

--- modules/report_mod.R
+++ modules/report_mod.R
@@ -31,7 +31,7 @@ report_ui <- function(id) {
 
 # Gene Text Input Module Server function
 
-report_server <- function(id) {
+report_html_server <- function(id) {
   moduleServer(
     id,
     function(input, output, session) {
@@ -122,4 +122,97 @@ report_server <- function(id) {
     }
   )
 }
+
+report_pdf_server <- function(id) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      
+      hostess <- Hostess$new("loader", infinite = TRUE, min = 20)
+      
+      output$dl <- downloadHandler(
+
+        # Create a file name using the response ID
+        filename = function() {
+          paste0(Sys.time(), "-KS-Report.html")
+        },
+        content = function(file) {
+
+          waiter_show(color = "#bdbaba",
+                      html = hostess$get_loader(
+                        text_color = "black",
+                        center_page = TRUE,
+                        svg = "assets/images/ks_new_logo.svg",
+                        progress_type = "fill",
+                        fill_direction = sample(c("btt"), 1)
+                        )
+                      )
+
+          hostess$start()
+
+          input_genes <- process_gene_input(input$genes)
+          
+          if(any(identical(input_genes, character(0)), input_genes == "")) {
+            shinyalert("Opps", "Enter a valid gene symbol", type = "error")
+            
+            hostess$close()
+            waiter_hide()
+            
+
+          }
+          
+          else {
+
+          temp_path1 <- file.path(tempdir(), "ks-report.Rmd")
+          file.copy(paste0("www/", "ks-report.Rmd"), temp_path1, overwrite = TRUE)
+          
+          temp_path2 <- file.path(tempdir(), "ks_new_logo.svg")
+          file.copy(paste0("www/", "assets/images/ks_new_logo.svg"), temp_path2, overwrite = TRUE)
+          
+          temp_path3 <- file.path(tempdir(), "string_embedded_network_v2.0.2.js")
+          file.copy("www/string_embedded_network_v2.0.2.js", temp_path3, overwrite = TRUE)
+          
+          param_list <- list(gene = input$genes, 
+                             brainrnaseq = input$brainseq_par,
+                             ilincs = input$ilincs_par,
+                             gwas = input$gwas_par,
+                             braincloud = input$braincloud_par,
+                             string = input$string_par,
+                             brainatlas = input$brainatlas_par,
+                             gtex = input$gtex_par
+                             )
+          
+          # Knit the document in its own environment, isolating its code from the Shiny app code.
+          # xfun::Rscript_call(
+          #   rmarkdown::render,
+          #   list(input = temp_path1, 
+          #        output_format = file,
+          #        params = param_list,
+          #        envir = new.env())
+          # )
+
+            rmarkdown::render(input = temp_path1,
+                              #output_format = rmarkdown::html_document(),
+                              output_file = file, # this lets the rendered report be accessed by download handler
+                              params = param_list,
+                              envir = new.env()
+            ) 
+          # rmarkdown::render(input = temp_path1,
+          #                   #output_format = rmarkdown::html_document(),
+          #                   output_file = file, # this lets the rendered report be accessed by download handler
+          #                   params = param_list,
+          #                   envir = new.env()
+          # )
+          
+          hostess$close()
+          waiter_hide()
+          
+        }
+        }
+      )
+      
+    }
+  )
+}
+
 
