5b8a0ca40d0691db45ac834d768a9c08c4ee1207
rela-v <rela.mngrbot@gmail.com>
2025-12-12 14:14:48 -0500

    frontend first commit

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+      class="max-w-sm rounded-lg shadow-2xl"
+    />
+    <div>
+      <h1 class="text-5xl font-bold">New release</h1>
+      <p class="py-6">
+        In-Silico Data Exploration of Biological Datasets.
+      </p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
